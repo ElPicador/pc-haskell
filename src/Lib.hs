82ac29c034e0dbfb6e166049204e42ac94dfdfb4
Rémy-Christophe Schermesser <git@schermesser.com>
2018-02-28 16:10:45 -0800

    more stuff

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,55 +1,61 @@
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
+
 module Lib
-    ( Result(..)
-    , pchar
-    , run
-    , andThen
-    , (.>>.)
-    , orElse
-    , (<|>)
-    , anyOf
-    , parseLowercase
-    , parseDigit
-    , mapP) where
+  ( Result(..)
+  , pchar
+  , run
+  , choice
+  , andThen
+  , orElse
+  , anyOf
+  , parseLowercase
+  , parseDigit
+  , mapP
+  , parseThreeDigitsAsStr
+  , parseThreeDigitsAsInt
+  , sequenceP
+  , many
+  ) where
 
-data Result a =
-    Success a |
-    Failure String
-    deriving (Eq, Show)
+data Result a
+  = Success a
+  | Failure String
+  deriving (Eq, Show)
 
-data Parser t =
-    Parser (String -> Result (t, String))
+newtype Parser t =
+  Parser (String -> Result (t, String))
 
 pchar :: Char -> Parser Char
 pchar char = Parser f
-    where
-        f [] = Failure "No more input"
-        f (x:xs) = if x == char
-                   then Success (char, xs)
-                   else Failure $ "Expecting " ++ show char ++ ". Got " ++ show x
+  where
+    f [] = Failure "No more input"
+    f (x:xs) =
+      if x == char
+        then Success (char, xs)
+        else Failure $ "Expecting " ++ show char ++ ". Got " ++ show x
 
 run :: Parser t -> String -> Result (t, String)
-run (Parser f) input = f input
+run (Parser f) = f
 
-andThen :: Parser t -> Parser t -> Parser (t, t)
+andThen :: Parser a -> Parser b -> Parser (a, b)
 andThen parser1 parser2 = Parser f
-    where
-        f input = case run parser1 input of
-                      Failure s -> Failure s
-                      Success (value1, remaining1) ->
-                          case run parser2 remaining1 of
-                              Failure s -> Failure s
-                              Success (value2, remaining2) -> Success ((value1, value2), remaining2)
-
-(.>>.) = andThen
+  where
+    f input =
+      case run parser1 input of
+        Failure s -> Failure s
+        Success (value1, remaining1) ->
+          case run parser2 remaining1 of
+            Failure s -> Failure s
+            Success (value2, remaining2) ->
+              Success ((value1, value2), remaining2)
 
 orElse :: Parser t -> Parser t -> Parser t
 orElse parser1 parser2 = Parser f
-    where
-        f input = case run parser1 input of
-            s@(Success _) -> s
-            Failure _ -> run parser2 input
-
-(<|>) = orElse
+  where
+    f input =
+      case run parser1 input of
+        s@(Success _) -> s
+        Failure _ -> run parser2 input
 
 choice :: [Parser t] -> Parser t
 choice = foldr1 orElse
@@ -58,22 +64,66 @@ anyOf :: [Char] -> Parser Char
 anyOf = choice . map pchar
 
 parseLowercase :: Parser Char
-parseLowercase = anyOf ['a'..'z']
+parseLowercase = anyOf ['a' .. 'z']
 
 parseDigit :: Parser Char
-parseDigit = anyOf ['0'..'9']
+parseDigit = anyOf ['0' .. '9']
 
 mapP :: (a -> b) -> Parser a -> Parser b
 mapP f parser = Parser innerF
-    where
-        innerF input = case run parser input of
-            Failure s -> Failure s
-            Success (value, remaining) -> Success (f value, remaining)
+  where
+    innerF input =
+      case run parser input of
+        Failure s -> Failure s
+        Success (value, remaining) -> Success (f value, remaining)
 
-(<!>) = mapP
-
-parseThreeDigitsAsStr :: () -> Parser String
+parseThreeDigitsAsStr :: Parser String
 parseThreeDigitsAsStr = mapP transformTuple tupleParser
-    where
-        tupleParser = parseDigit .>>. parseDigit .>>. parseDigit
-        transformTuple ((c1, c2), c3) = [c1, c2, c3]
+  where
+    tupleParser = parseDigit `andThen` parseDigit `andThen` parseDigit
+    transformTuple ((c1, c2), c3) = [c1, c2, c3]
+
+parseThreeDigitsAsInt :: Parser Integer
+parseThreeDigitsAsInt = mapP read parseThreeDigitsAsStr
+
+returnP :: a -> Parser a
+returnP x = Parser f
+  where
+    f input = Success (x, input)
+
+applyP :: Parser (a -> b) -> Parser a -> Parser b
+applyP fP xP = mapP lambda parsers
+  where
+    parsers = fP `andThen` xP
+    lambda (f, x) = f x
+
+
+lift2 :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
+lift2 f aP bP = returnP f `applyP` aP `applyP` bP
+
+addP :: Parser Integer -> Parser Integer -> Parser Integer
+addP = lift2 (+)
+
+sequenceP :: [Parser a] -> Parser [a]
+sequenceP [] = returnP []
+sequenceP (head:tail) = consP head (sequenceP tail)
+  where
+    consP = lift2 (:)
+
+pstring :: String -> Parser String
+pstring str = sequenceP $ map pchar str
+
+many :: Parser a -> Parser [a]
+many parser = Parser innerFn
+  where
+    innerFn input = Success (parseZeroOrMore parser input)
+    parseZeroOrMore :: Parser a -> String -> ([a], String)
+    parseZeroOrMore parser input = case run parser input of
+        Failure e -> ([], input)
+        Success (firstValue, inputAfterFirstParse) ->
+          (values, remainingInput)
+            where
+              (subsequentValues, remainingInput) = parseZeroOrMore parser inputAfterFirstParse
+              values = firstValue : subsequentValues
+
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,31 +1,53 @@
+import Lib
+  ( Result(..)
+  , parseDigit
+  , parseLowercase
+  , parseThreeDigitsAsInt
+  , parseThreeDigitsAsStr
+  , pchar
+  , andThen
+  , orElse
+  , run
+  , sequenceP
+  , many
+  )
+
 import Test.Hspec
-import Lib (pchar, run, (.>>.), (<|>), Result(..), parseLowercase, parseDigit)
 
 pcharA = pchar 'A'
+
 pcharB = pchar 'B'
-pcharAThenB = pcharA .>>. pcharB
-pcharAOrB = pcharA <|> pcharB
+
+pcharAThenB = pcharA `andThen` pcharB
+
+pcharAOrB = pcharA `orElse` pcharB
+
+manyA = many $ pchar 'A'
+
+parsers = [ pchar 'A', pchar 'B', pchar 'C' ]
+combined = sequenceP parsers
 
 main :: IO ()
-main = hspec $ do
-  describe "pchar" $ do
-    it "parses 'ABC'" $ do
-      run pcharA "ABC" `shouldBe` Success ('A', "BC")
+main =
+  hspec $ do
+    describe "pchar" $ do
+      it "parses 'ABC'" $ do
+        run pcharA "ABC" `shouldBe` Success ('A', "BC")
 
-    it "parses 'ZBC'" $ do
-      run pcharA "ZBC" `shouldBe` Failure "Expecting 'A'. Got 'Z'"
+      it "parses 'ZBC'" $ do
+        run pcharA "ZBC" `shouldBe` Failure "Expecting 'A'. Got 'Z'"
 
-  describe "andThen" $ do
-    it "parses 'ABC'" $ do
-      run pcharAThenB "ABC" `shouldBe` Success (('A', 'B'), "C")
+    describe "andThen" $ do
+      it "parses 'ABC'" $ do
+        run pcharAThenB "ABC" `shouldBe` Success (('A', 'B'), "C")
 
-    it "parses 'ZBC'" $ do
-      run pcharAThenB "ZBC" `shouldBe` Failure "Expecting 'A'. Got 'Z'"
+      it "parses 'ZBC" $ do
+        run pcharAThenB "ZBC" `shouldBe` Failure "Expecting 'A'. Got 'Z'"
 
-    it "parses 'AZC'" $ do
-      run pcharAThenB "AZC" `shouldBe` Failure "Expecting 'B'. Got 'Z'"
+      it "parses 'AZC'" $ do
+        run pcharAThenB "AZC" `shouldBe` Failure "Expecting 'B'. Got 'Z'"
 
-  describe "orElse" $ do
+    describe "orElse" $ do
       it "parses 'ABC'" $ do
         run pcharAOrB "ABC" `shouldBe` Success ('A', "BC")
 
@@ -35,10 +57,35 @@ main = hspec $ do
       it "parses 'ZBC'" $ do
         run pcharAOrB "ZBC" `shouldBe` Failure "Expecting 'B'. Got 'Z'"
 
-  describe "parseLowercase" $ do
-    it "parses 'aBC'" $ do
+    describe "parseLowercase" $ do
+      it "parses 'aBC'" $ do
         run parseLowercase "aBC" `shouldBe` Success ('a', "BC")
 
-  describe "parseDigit" $ do
-    it "parses '1ABC'"$ do
+    describe "parseDigit" $ do
+      it "parses '1ABC'" $ do
         run parseDigit "1ABC" `shouldBe` Success ('1', "ABC")
+
+    describe "parseThreeDigitsAsStr" $ do
+      it "parses '123A'" $ do
+        run parseThreeDigitsAsStr "123A" `shouldBe` Success ("123", "A")
+
+    describe "parseThreeDigitsAsInt" $ do
+      it "parses '123A'" $ do
+        run parseThreeDigitsAsInt "123A" `shouldBe` Success (123, "A")
+
+    describe "sequence" $ do
+      it "parses 'ABCD'"$ do
+        run combined "ABCD" `shouldBe` Success (['A', 'B', 'C'], "D")
+
+    describe "many" $ do
+      it "parses 'ABCD'" $ do
+        run manyA "ABCD" `shouldBe` Success (['A'], "BCD")
+
+      it "parses 'AACD'" $ do
+        run manyA "AACD" `shouldBe` Success (['A', 'A'], "CD")
+
+      it "parses 'AAAD'" $ do
+        run manyA "AAAD" `shouldBe` Success (['A', 'A', 'A'], "CD")
+
+      it "parses '|BCD'" $ do
+        run manyA "|BCD" `shouldBe` Success ([], "|BCD")
